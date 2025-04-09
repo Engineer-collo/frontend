5ee94f76144166fb4becf8127855ace95d9f186a
Engineer-Collo <collinslikhomba@gmail.com>
2025-04-09 18:49:38 +0300

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
+  
 import Navbar from "./Navbar";
-import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Comments from "./components/Comments";
-import Comment from "./Comment";
 import Posts from "./components/Posts";
+import Profile from "./components/Profile";
+import Likes from "./components/Likes";
+
+import Home from "./components/Home";
+import Comment from "./Comment";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      
-      {/* Here's your link to Comment page */}
-      <div className="p-4">
-        <Link to="/Comments" className="text-blue-500 underline">
-          Comments
-        </Link>
+      <Navbar/>
 
-        <Link to="/Comment" className="text-blue-500 underline">
-          Comment
-        </Link>
 
-        <Link to="/Posts" className="text-blue-500 underline">
-          Post
-        </Link>
 
-      </div>
       <Routes>
-      <Route path="/posts" element={<Posts />} />
-      <Route path="/Comment" element={<Comment />} />
-        <Route path="/Comments" element={<Comments />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+          <Route path="/Likes" element={<Likes />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/Comments" element={<Comments />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home/>}/>
+          <Route path="Comment" element={<Comment/>}/>
       </Routes>
     </Router>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,26 +1,32 @@
-
+import { Link } from "react-router-dom";
 
 function Home() {
   return(
 
     <>
-    
+
     <section>
       <h1 className="text-center pt-4">
-        <strong> <i> <h1 className="text-blue-500 text-2xl"> Hellow this is pixi-fy homepage</h1></i></strong>
+        <strong> <i> <h1 className="text-blue-500 text-2xl "> Hellow this is pixi-fy homepage</h1></i></strong>
       </h1>
     </section>
      <section className=" flex gap-20 justify-between justify-center">
-      <div>
+      <div className="">
         <h1>
-          <i className="text-xl">Dashboard</i>
+          <i className="text-xl border">Dashboard</i>
         </h1>
       </div>
-        <div>
+      <Link to="/Profile" className="text-blue-500 ">Profile</Link>
+      <Link to="/comment" className="text-blue-500  ">Comment</Link>
+      <Link to="/Likkes" className="text-blue-500  "> <img src="/like.png" alt="Like" className="rounded-xl" /></Link>
+      <Link to="/Posts" className="text-blue-500  ">Posts</Link>
+      <Link to="/comments" className="text-blue-500 ">Comments</Link>
+
+        <div className="">
           
         </div>
 
-        <div>
+        <div className="">
           <h1>
             
           </h1>
@@ -29,7 +35,7 @@ function Home() {
      </section>
 
      
-      
+
     </>
   );
 }
